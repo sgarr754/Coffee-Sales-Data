1f7febb413bed91c5d043e0fbb31ae2ac4866313
sgarr754 <sgarr229@gmail.com>
2025-08-28 14:48:14 -0600

    Adding analysis work

--- src/analysis.py
+++ src/analysis.py
@@ -12,7 +12,18 @@ def analysis(df):
     # Months = (df[["Month_name", 'Monthsort']].drop_duplicates().sort_values('Monthsort'))
     # print(Months)
 
-    # How many drinks are bought in each month
+    # How many drinks are bought in each month?
     monthly_drinks = df.groupby(['Monthsort', 'Month_name']).size().sort_index(level='Monthsort')
 
-    return {"prices": drinks_and_price, "time_counts": drinks_TOD, "monthly_drinks": monthly_drinks}
+    # How many transactions were through card vs. cash?
+    payment_type = df['cash_type'].value_counts()
+
+    # What are the top-selling drinks and how much have they sold?
+    top_drinks = df.groupby('coffee_name')['money'].agg(['sum','count']).sort_values(by='sum', ascending=False)
+
+
+    return {"prices": drinks_and_price,
+            "time_counts": drinks_TOD,
+            "monthly_drinks": monthly_drinks,
+            "payment_type": payment_type,
+            "top_drinks": top_drinks}
